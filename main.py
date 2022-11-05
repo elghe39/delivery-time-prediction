7f62fa6c6c5ea5744f496035bc7a6960e0592692
Nguyễn Mạnh Đức <ducnguyenw02@gmail.com>
2022-11-05 21:12:32 +0700

    main

---
+++ main.py
@@ -0,0 +1,29 @@
+import create_data
+import os
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.offline as py
+import seaborn as sns
+
+import matplotlib.ticker as mtick
+
+def main():
+
+    # Tạo file dữ liệu mới và đọc file dữ liệu csv được khởi tạo 
+    # path = "D:/Github/Predict_the_time_of_arrival_for_the_delivery_persons/dataset/data"
+    # os.chdir(path)
+    # df = pd.read_csv(create_data.main(path))
+
+    # Đọc file dữ liệu có sẵn 
+    df = pd.read_csv("D:/Github/Predict_the_time_of_arrival_for_the_delivery_persons/dataset/data_new/dataset_new.csv")
+    
+    # Pre-processing 
+
+    # EDA + deploy 
+
+    # Model 
+
+if __name__ == '__main__':
+    main()
